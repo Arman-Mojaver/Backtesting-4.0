8a88dfa02c8266c9b9bc1514a1eb2674e72be079
Arman Mojaver <arman_mojaver@hotmail.com>
2024-11-24 20:51:45 +0100

    testing_utils: add lists_are_equal()

--- testing_utils/dict_utils.py
+++ testing_utils/dict_utils.py
@@ -3,6 +3,13 @@ from __future__ import annotations
 from typing import Any
 
 
+def lists_are_equal(list_1: list[Any], list_2: list[Any]) -> bool:
+    return all(
+        any(item1.to_dict() == item2.to_dict() for item2 in list_2)
+        for item1 in list_1
+    )
+
+
 def dicts_by_key_are_equal(dict_1: dict[Any, Any], dict_2: dict[Any, Any]) -> bool:
     if dict_1.keys() != dict_2.keys():
         return False
@@ -17,17 +24,4 @@ def dicts_multi_by_key_are_equal(
     if dict_1.keys() != dict_2.keys():
         return False
 
-    for key in dict_1:
-        list_1, list_2 = dict_1[key], dict_2[key]
-
-        for item_list_1 in list_1:
-            has_equal = False
-            for item_list_2 in list_2:
-                if item_list_1.to_dict() == item_list_2.to_dict():
-                    has_equal = True
-                    break
-
-            if not has_equal:
-                return False
-
-    return True
+    return all(lists_are_equal(dict_1[key], dict_2[key]) for key in dict_1)
