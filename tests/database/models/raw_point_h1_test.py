77abb18f3dfbcdf83c159b9700c70597c4983073
Arman Mojaver <arman_mojaver@hotmail.com>
2024-12-05 11:17:56 +0100

    tests: add raw_point_h1 model tests

---
+++ tests/database/models/raw_point_h1_test.py
@@ -0,0 +1,57 @@
+import pytest
+from sqlalchemy.exc import IntegrityError
+
+from database.models import RawPointH1
+
+
+def test_create_point(session):
+    point_data = {
+        "datetime": "2023-11-13 00:00:00",
+        "instrument": "EURUSD",
+        "open": 1.06751,
+        "high": 1.0706,
+        "low": 1.06648,
+        "close": 1.06981,
+        "volume": 47554,
+    }
+
+    point = RawPointH1(**point_data)
+
+    session.add(point)
+    session.commit()
+
+    assert point.id
+    assert point.to_dict() == point_data
+
+    session.delete(point)
+    session.commit()
+
+
+def test_instrument_datetime_unique_constraint(session):
+    point_data_1 = {
+        "datetime": "2023-11-13 00:00:00",
+        "instrument": "EURUSD",
+        "open": 1.06751,
+        "high": 1.0706,
+        "low": 1.06648,
+        "close": 1.06981,
+        "volume": 47554,
+    }
+
+    point_data_2 = {
+        "datetime": "2023-11-13 00:00:00",
+        "instrument": "EURUSD",
+        "open": 1.06916,
+        "high": 1.08872,
+        "low": 1.06916,
+        "close": 1.08782,
+        "volume": 79728,
+    }
+
+    point_1 = RawPointH1(**point_data_1)
+    point_2 = RawPointH1(**point_data_2)
+
+    session.add_all([point_1, point_2])
+
+    with pytest.raises(IntegrityError):
+        session.commit()
