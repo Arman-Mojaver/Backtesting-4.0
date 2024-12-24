c77a9d3747bf65b9df2b293797ae2ae44e88611b
Arman Mojaver <arman_mojaver@hotmail.com>
2024-12-24 11:59:51 +0000

    testing_utils: convert mutable types to immutable in list_of_dicts_are_equal()

--- testing_utils/dict_utils.py
+++ testing_utils/dict_utils.py
@@ -26,10 +26,26 @@ def dicts_multi_by_key_are_equal(
     return all(lists_are_equal(dict_1[key], dict_2[key]) for key in dict_1)
 
 
+def _convert_lists_to_tuples(data: dict[str, Any]):
+    if isinstance(data, dict):
+        return {key: _convert_lists_to_tuples(value) for key, value in data.items()}
+
+    if isinstance(data, list):
+        return tuple(_convert_lists_to_tuples(item) for item in data)
+
+    if isinstance(data, (tuple, set)):
+        return type(data)(_convert_lists_to_tuples(item) for item in data)
+
+    return data
+
+
 def list_of_dicts_are_equal(
     list_1: list[dict[str, Any]],
     list_2: list[dict[str, Any]],
 ) -> bool:
-    return {frozenset(i.items()) for i in list_1} == {
-        frozenset(i.items()) for i in list_2
+    converted_list_1 = [_convert_lists_to_tuples(i) for i in list_1]
+    converted_list_2 = [_convert_lists_to_tuples(i) for i in list_2]
+
+    return {frozenset(i.items()) for i in converted_list_1} == {
+        frozenset(i.items()) for i in converted_list_2
     }
