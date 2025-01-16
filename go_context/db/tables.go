f970c7ec246f781713ad4738f933a65d4d042313
Arman Mojaver <arman_mojaver@hotmail.com>
2025-01-16 20:44:14 +0000

    go_context: add CreateResampledPointD1Table

--- go_context/db/tables.go
+++ go_context/db/tables.go
@@ -9,6 +9,10 @@ import (
 )
 
 func CreateTables(conn *sql.DB) error {
+	if err := CreateResampledPointD1Table(conn); err != nil {
+		return fmt.Errorf("failed to create resampled_point_d1 table: %w", err)
+	}
+
 	if err := CreateLongOperationPointTable(conn); err != nil {
 		return fmt.Errorf("failed to create long operation point table: %w", err)
 	}
@@ -25,6 +29,27 @@ func CreateTables(conn *sql.DB) error {
 		return fmt.Errorf("failed to create strategy table: %w", err)
 	}
 
+	return nil
+}
+
+func CreateResampledPointD1Table(conn *sql.DB) error {
+	CreateResampledPointD1TableStatement := `
+	CREATE TABLE IF NOT EXISTS resampled_point_d1 (
+		id SERIAL PRIMARY KEY,
+		datetime DATE NOT NULL,
+		instrument VARCHAR NOT NULL,
+		open FLOAT NOT NULL,
+		high FLOAT NOT NULL,
+		low FLOAT NOT NULL,
+		close FLOAT NOT NULL,
+		volume INTEGER NOT NULL,
+		high_low_order VARCHAR NOT NULL
+	);`
+
+	_, err := conn.Exec(CreateResampledPointD1TableStatement)
+	if err != nil {
+		return err
+	}
 	return nil
 }
 
