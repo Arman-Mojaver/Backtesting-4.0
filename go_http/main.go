8dbeeee1baabe69ceff36962b46314d6aa821f43
Arman Mojaver <arman_mojaver@hotmail.com>
2025-03-08 07:44:44 +0100

    go_http: change log name to go-http.log, add .gitkeep file

--- go_http/main.go
+++ go_http/main.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	// Setup logging
-	file, err := os.OpenFile("logs/bt.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+	file, err := os.OpenFile("logs/go-http.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatalf("Failed to open log file: %s", err)
 	}
