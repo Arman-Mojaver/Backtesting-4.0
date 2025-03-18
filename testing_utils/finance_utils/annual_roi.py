7d080d88fcb20363f9c574645df650e1f0c40cf3
Arman Mojaver <arman_mojaver@hotmail.com>
2025-03-18 11:45:46 +0100

    testing_utils: add datetime.date support for calculate_annual_roi()

--- testing_utils/finance_utils/annual_roi.py
+++ testing_utils/finance_utils/annual_roi.py
@@ -1,7 +1,18 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from testing_utils.finance_utils.utils import get_difference_in_years
 
+if TYPE_CHECKING:
+    import datetime
 
-def calculate_annual_roi(start_date: str, end_date: str, global_roi: float) -> float:
+
+def calculate_annual_roi(
+    start_date: str | datetime.date,
+    end_date: str | datetime.date,
+    global_roi: float,
+) -> float:
     years = get_difference_in_years(start_date, end_date)
     global_accumulated_value = 1 + global_roi / 100
     annual_accumulated_value = global_accumulated_value ** (1 / years)
--- testing_utils/finance_utils/utils.py
+++ testing_utils/finance_utils/utils.py
@@ -1,8 +1,24 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+
 from utils.date_utils import string_to_datetime
 
+if TYPE_CHECKING:
+    import datetime
 
-def get_difference_in_years(date_1: str, date_2: str) -> float:
-    datetime_1, datetime_2 = string_to_datetime(date_1), string_to_datetime(date_2)
+
+def get_difference_in_years(
+    date_1: str | datetime.date,
+    date_2: str | datetime.date,
+) -> float:
+    datetime_1 = date_1
+    if isinstance(date_1, str):
+        datetime_1 = string_to_datetime(date_1)
+
+    datetime_2 = date_2
+    if isinstance(date_2, str):
+        datetime_2 = string_to_datetime(date_2)
 
     # Using 365.25 to account for leap years
     return round(abs((datetime_1 - datetime_2).days) / 365.25, 2)
