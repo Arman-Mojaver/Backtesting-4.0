631ecbc742eb5f5a59da5f6d5fb02d2edaf330d2
Arman Mojaver <arman_mojaver@hotmail.com>
2025-03-24 18:53:57 +0100

    go_http: put process_strategies code in separate file

--- go_http/main.go
+++ go_http/main.go
@@ -4,11 +4,8 @@ import (
 	"encoding/json"
 	"io"
 	"log"
-	"math"
 	"net/http"
 	"os"
-	"sort"
-	"time"
 )
 
 func main() {
@@ -50,76 +47,6 @@ func pingHandler(w http.ResponseWriter, r *http.Request) {
 	log.Println("GET /ping")
 }
 
-func stringToDatetime(dateStr string) (time.Time, error) {
-	layout := "2006-01-02" // Define the expected date format
-	return time.Parse(layout, dateStr)
-}
-
-func getDifferenceInYears(date1, date2 string) float64 {
-	const daysInYear = 365.25
-
-	parsedDate1, err := stringToDatetime(date1)
-	if err != nil {
-		panic(err)
-	}
-	parsedDate2, err := stringToDatetime(date2)
-	if err != nil {
-		panic(err)
-	}
-
-	duration := parsedDate1.Sub(parsedDate2).Hours() / 24
-	return math.Round(math.Abs(duration)/daysInYear*100) / 100
-}
-
-// Request Structs
-
-type OperationPoint struct {
-	ID                        int     `json:"id"`
-	Instrument                string  `json:"instrument"`
-	Datetime                  string  `json:"datetime"`
-	Result                    int     `json:"result"`
-	Tp                        int     `json:"tp"`
-	Sl                        int     `json:"sl"`
-	Risk                      float64 `json:"risk"`
-	MoneyManagementStrategyID int     `json:"money_management_strategy_id"`
-}
-
-type OperationPoints struct {
-	LongOperationPoints  map[string]OperationPoint `json:"long_operation_points"`
-	ShortOperationPoints map[string]OperationPoint `json:"short_operation_points"`
-}
-
-type Signals struct {
-	LongSignals  []string `json:"long_signals"`
-	ShortSignals []string `json:"short_signals"`
-}
-
-type RequestPayload struct {
-	OperationPoints map[int]OperationPoints `json:"operation_points"`
-	Signals         map[int]Signals         `json:"signals"`
-	StartDate       string                  `json:"start_date"`
-	EndDate         string                  `json:"end_date"`
-}
-
-// Response Structs
-type StrategyData struct {
-	AnnualROI                 float64 `json:"annual_roi"`
-	MaxDrawDown               float64 `json:"max_draw_down"`
-	AnnualOperationCount      float64 `json:"annual_operation_count"`
-	MoneyManagementStrategyID int     `json:"money_management_strategy_id"`
-	IndicatorID               int     `json:"indicator_id"`
-}
-
-type StrategyItem struct {
-	StrategyData           StrategyData `json:"strategy_data"`
-	LongOperationPointIds  []int        `json:"long_operation_point_ids"`
-	ShortOperationPointIds []int        `json:"short_operation_point_ids"`
-}
-
-type Response struct {
-	Data []StrategyItem `json:"data"`
-}
-
 func processStrategiesHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
 		response := map[string]string{"error": "Method Not Allowed"}
@@ -139,52 +66,7 @@ func processStrategiesHandler(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	var strategyItems []StrategyItem
-	for moneyManagementStrategyID, operationPoints := range payload.OperationPoints {
-		for indicatorID, signals := range payload.Signals {
-			longOperationPoints := []OperationPoint{}
-			longOperationPointIDs := []int{}
-			for _, signal := range signals.LongSignals {
-				longOperationPoint := operationPoints.LongOperationPoints[signal]
-				longOperationPoints = append(longOperationPoints, longOperationPoint)
-				longOperationPointIDs = append(longOperationPointIDs, longOperationPoint.ID)
-			}
-
-			shortOperationPoints := []OperationPoint{}
-			shortOperationPointIDs := []int{}
-			for _, signal := range signals.ShortSignals {
-				shortOperationPoint := operationPoints.ShortOperationPoints[signal]
-				shortOperationPoints = append(shortOperationPoints, shortOperationPoint)
-				shortOperationPointIDs = append(shortOperationPointIDs, shortOperationPoint.ID)
-			}
-
-			operationPoints := append(longOperationPoints, shortOperationPoints...)
-			sort.Slice(operationPoints, func(i, j int) bool {
-				dateFormat := "2001-01-13"
-
-				ti, _ := time.Parse(dateFormat, operationPoints[i].Datetime)
-				tj, _ := time.Parse(dateFormat, operationPoints[j].Datetime)
-				return ti.Before(tj)
-			})
-			differenceInYears := getDifferenceInYears(payload.StartDate, payload.EndDate)
-
-			strategy := StrategyData{
-				AnnualROI:            0.0,
-				MaxDrawDown:          0.0,
-				AnnualOperationCount: float64(len(operationPoints)) / differenceInYears,
-				MoneyManagementStrategyID: moneyManagementStrategyID,
-				IndicatorID: indicatorID,
-			}
-			strategyResponse := StrategyItem{
-				StrategyData:           strategy,
-				LongOperationPointIds:  longOperationPointIDs,
-				ShortOperationPointIds: shortOperationPointIDs,
-			}
-
-			strategyItems = append(strategyItems, strategyResponse)
-		}
-	}
-
+	strategyItems := ProcessStrategies(payload)
 	response := Response{Data: strategyItems}
 
 	w.Header().Set("Content-Type", "application/json")
