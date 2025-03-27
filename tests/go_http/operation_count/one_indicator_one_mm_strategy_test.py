40b167c4d62ba92bec8bb8c6209068947da93680
Arman Mojaver <arman_mojaver@hotmail.com>
2025-03-27 10:23:08 +0100

    tests: add partial operation point selection tests

--- tests/go_http/operation_count/one_indicator_one_mm_strategy_test.py
+++ tests/go_http/operation_count/one_indicator_one_mm_strategy_test.py
@@ -292,3 +292,181 @@ def test_annual_operation_count_with_short_2(
         response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
     )
     assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
+
+
+# Partial Operation Point selection
+def test_annual_operation_count_with_partial_operation_point_selection_1(  # noqa: PLR0913
+    long_operation_point,
+    long_operation_point_2,
+    short_operation_point,
+    short_operation_point_2,
+    endpoint,
+    session,
+):
+    body = {
+        "operation_points": {
+            10: {  # money_management_strategy_id
+                "long_operation_points": {
+                    datetime_to_string(
+                        long_operation_point.datetime
+                    ): long_operation_point.to_request_format(),
+                    datetime_to_string(
+                        long_operation_point_2.datetime
+                    ): long_operation_point_2.to_request_format(),
+                },
+                "short_operation_points": {
+                    datetime_to_string(
+                        short_operation_point.datetime
+                    ): short_operation_point.to_request_format(),
+                    datetime_to_string(
+                        short_operation_point_2.datetime
+                    ): short_operation_point_2.to_request_format(),
+                },
+            },
+        },
+        "signals": {
+            20: {  # indicator_id
+                "long_signals": [
+                    datetime_to_string(long_operation_point.datetime),
+                    datetime_to_string(long_operation_point_2.datetime),
+                ],
+                "short_signals": [datetime_to_string(short_operation_point.datetime)],
+            },
+        },
+        "start_date": datetime_to_string(long_operation_point.datetime),
+        "end_date": datetime_to_string(short_operation_point_2.datetime),
+    }
+
+    response = requests.post(
+        url=endpoint("process_strategies"),
+        json=body,
+        timeout=5,
+    )
+
+    response_content = parse_response(response)
+
+    assert len(response_content["data"]) == 1
+    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 1.5
+    assert (
+        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
+    )
+    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
+
+
+def test_annual_operation_count_with_partial_operation_point_selection_2(  # noqa: PLR0913
+    long_operation_point,
+    long_operation_point_2,
+    short_operation_point,
+    short_operation_point_2,
+    endpoint,
+    session,
+):
+    body = {
+        "operation_points": {
+            10: {  # money_management_strategy_id
+                "long_operation_points": {
+                    datetime_to_string(
+                        long_operation_point.datetime
+                    ): long_operation_point.to_request_format(),
+                    datetime_to_string(
+                        long_operation_point_2.datetime
+                    ): long_operation_point_2.to_request_format(),
+                },
+                "short_operation_points": {
+                    datetime_to_string(
+                        short_operation_point.datetime
+                    ): short_operation_point.to_request_format(),
+                    datetime_to_string(
+                        short_operation_point_2.datetime
+                    ): short_operation_point_2.to_request_format(),
+                },
+            },
+        },
+        "signals": {
+            20: {  # indicator_id
+                "long_signals": [datetime_to_string(long_operation_point.datetime)],
+                "short_signals": [
+                    datetime_to_string(short_operation_point.datetime),
+                    datetime_to_string(short_operation_point_2.datetime),
+                ],
+            },
+        },
+        "start_date": datetime_to_string(long_operation_point.datetime),
+        "end_date": datetime_to_string(short_operation_point_2.datetime),
+    }
+
+    response = requests.post(
+        url=endpoint("process_strategies"),
+        json=body,
+        timeout=5,
+    )
+
+    response_content = parse_response(response)
+
+    assert len(response_content["data"]) == 1
+    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 1.5
+    assert (
+        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
+    )
+    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
+
+
+def test_annual_operation_count_with_partial_operation_point_selection_3(  # noqa: PLR0913
+    long_operation_point,
+    long_operation_point_2,
+    short_operation_point,
+    short_operation_point_2,
+    endpoint,
+    session,
+):
+    body = {
+        "operation_points": {
+            10: {  # money_management_strategy_id
+                "long_operation_points": {
+                    datetime_to_string(
+                        long_operation_point.datetime
+                    ): long_operation_point.to_request_format(),
+                    datetime_to_string(
+                        long_operation_point_2.datetime
+                    ): long_operation_point_2.to_request_format(),
+                },
+                "short_operation_points": {
+                    datetime_to_string(
+                        short_operation_point.datetime
+                    ): short_operation_point.to_request_format(),
+                    datetime_to_string(
+                        short_operation_point_2.datetime
+                    ): short_operation_point_2.to_request_format(),
+                },
+            },
+        },
+        "signals": {
+            20: {  # indicator_id
+                "long_signals": [
+                    datetime_to_string(long_operation_point.datetime),
+                    datetime_to_string(long_operation_point_2.datetime),
+                ],
+                "short_signals": [
+                    datetime_to_string(short_operation_point.datetime),
+                    datetime_to_string(short_operation_point_2.datetime),
+                ],
+            },
+        },
+        "start_date": datetime_to_string(long_operation_point.datetime),
+        "end_date": datetime_to_string(short_operation_point_2.datetime),
+    }
+
+    response = requests.post(
+        url=endpoint("process_strategies"),
+        json=body,
+        timeout=5,
+    )
+
+    response_content = parse_response(response)
+
+    assert len(response_content["data"]) == 1
+    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 2.0
+    assert (
+        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
+    )
+    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
