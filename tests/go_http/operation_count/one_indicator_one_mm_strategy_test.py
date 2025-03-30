0f1875ac42c965a834a63f6ba934f4285c1739f0
Arman Mojaver <arman_mojaver@hotmail.com>
2025-03-30 12:05:59 +0200

    tests: refactor annual_operation_count tests

--- tests/go_http/operation_count/one_indicator_one_mm_strategy_test.py
+++ tests/go_http/operation_count/one_indicator_one_mm_strategy_test.py
@@ -1,472 +1,61 @@
+import pytest
 import requests
 
 from testing_utils.http_utils import parse_response
-from utils.date_utils import datetime_to_string
+from testing_utils.request_body_factory import ProcessStrategiesRequestBodyFactory
 
 INSTRUMENT = "EURUSD"
 
 
-# 1 Year
-
-
-def test_annual_operation_count_with_long_and_short(
-    long_operation_point,
-    short_operation_point,
+@pytest.mark.parametrize(
+    (
+        "end_date",
+        "long_signals_counts",
+        "short_signals_counts",
+        "expected_result",
+    ),
+    [
+        ("2025-01-01", [1], [1], 2.0),
+        ("2025-01-01", [1], [0], 1.0),
+        ("2025-01-01", [0], [1], 1.0),
+        ("2026-01-01", [1], [1], 1.0),
+        ("2026-01-01", [1], [0], 0.5),
+        ("2026-01-01", [0], [1], 0.5),
+        ("2026-01-01", [2], [1], 1.5),
+        ("2026-01-01", [1], [2], 1.5),
+        ("2026-01-01", [2], [2], 2.0),
+    ],
+)
+def test_annual_operation_count(
+    end_date,
+    long_signals_counts,
+    short_signals_counts,
+    expected_result,
     endpoint,
-    session,
 ):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point.datetime
-                    ): short_operation_point.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [datetime_to_string(long_operation_point.datetime)],
-                "short_signals": [datetime_to_string(short_operation_point.datetime)],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point.datetime),
-    }
+    request_body = ProcessStrategiesRequestBodyFactory(
+        instrument=INSTRUMENT,
+        mm_strategy_count=1,
+        start_date="2024-01-01",
+        long_signals_counts=long_signals_counts,
+        short_signals_counts=short_signals_counts,
+        end_date=end_date,
+    )
 
     response = requests.post(
         url=endpoint("process_strategies"),
-        json=body,
+        json=request_body.generate_body(),
         timeout=5,
     )
 
     response_content = parse_response(response)
 
     assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 2.0
     assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
+        response_content["data"][0]["strategy_data"]["annual_operation_count"]
+        == expected_result
     )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
-
-
-def test_annual_operation_count_with_long(
-    long_operation_point,
-    short_operation_point,
-    endpoint,
-    session,
-):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point.datetime
-                    ): short_operation_point.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [datetime_to_string(long_operation_point.datetime)],
-                "short_signals": [],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point.datetime),
-    }
-
-    response = requests.post(
-        url=endpoint("process_strategies"),
-        json=body,
-        timeout=5,
-    )
-
-    response_content = parse_response(response)
-
-    assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 1.0
     assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
+        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 1
     )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
-
-
-def test_annual_operation_count_with_short(
-    long_operation_point,
-    short_operation_point,
-    endpoint,
-    session,
-):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point.datetime
-                    ): short_operation_point.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [],
-                "short_signals": [datetime_to_string(short_operation_point.datetime)],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point.datetime),
-    }
-
-    response = requests.post(
-        url=endpoint("process_strategies"),
-        json=body,
-        timeout=5,
-    )
-
-    response_content = parse_response(response)
-
-    assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 1.0
-    assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
-    )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
-
-
-# 2 Years
-
-
-def test_annual_operation_count_with_long_and_short_2(
-    long_operation_point,
-    short_operation_point_2,
-    endpoint,
-    session,
-):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point_2.datetime
-                    ): short_operation_point_2.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [datetime_to_string(long_operation_point.datetime)],
-                "short_signals": [datetime_to_string(short_operation_point_2.datetime)],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point_2.datetime),
-    }
-
-    response = requests.post(
-        url=endpoint("process_strategies"),
-        json=body,
-        timeout=5,
-    )
-
-    response_content = parse_response(response)
-
-    assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 1.0
-    assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
-    )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
-
-
-def test_annual_operation_count_with_long_2(
-    long_operation_point,
-    short_operation_point_2,
-    endpoint,
-    session,
-):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point_2.datetime
-                    ): short_operation_point_2.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [datetime_to_string(long_operation_point.datetime)],
-                "short_signals": [],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point_2.datetime),
-    }
-
-    response = requests.post(
-        url=endpoint("process_strategies"),
-        json=body,
-        timeout=5,
-    )
-
-    response_content = parse_response(response)
-
-    assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 0.5
-    assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
-    )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
-
-
-def test_annual_operation_count_with_short_2(
-    long_operation_point,
-    short_operation_point_2,
-    endpoint,
-    session,
-):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point_2.datetime
-                    ): short_operation_point_2.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [],
-                "short_signals": [datetime_to_string(short_operation_point_2.datetime)],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point_2.datetime),
-    }
-
-    response = requests.post(
-        url=endpoint("process_strategies"),
-        json=body,
-        timeout=5,
-    )
-
-    response_content = parse_response(response)
-
-    assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 0.5
-    assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
-    )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
-
-
-# Partial Operation Point selection
-def test_annual_operation_count_with_partial_operation_point_selection_1(  # noqa: PLR0913
-    long_operation_point,
-    long_operation_point_2,
-    short_operation_point,
-    short_operation_point_2,
-    endpoint,
-    session,
-):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                    datetime_to_string(
-                        long_operation_point_2.datetime
-                    ): long_operation_point_2.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point.datetime
-                    ): short_operation_point.to_request_format(),
-                    datetime_to_string(
-                        short_operation_point_2.datetime
-                    ): short_operation_point_2.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [
-                    datetime_to_string(long_operation_point.datetime),
-                    datetime_to_string(long_operation_point_2.datetime),
-                ],
-                "short_signals": [datetime_to_string(short_operation_point.datetime)],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point_2.datetime),
-    }
-
-    response = requests.post(
-        url=endpoint("process_strategies"),
-        json=body,
-        timeout=5,
-    )
-
-    response_content = parse_response(response)
-
-    assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 1.5
-    assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
-    )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
-
-
-def test_annual_operation_count_with_partial_operation_point_selection_2(  # noqa: PLR0913
-    long_operation_point,
-    long_operation_point_2,
-    short_operation_point,
-    short_operation_point_2,
-    endpoint,
-    session,
-):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                    datetime_to_string(
-                        long_operation_point_2.datetime
-                    ): long_operation_point_2.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point.datetime
-                    ): short_operation_point.to_request_format(),
-                    datetime_to_string(
-                        short_operation_point_2.datetime
-                    ): short_operation_point_2.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [datetime_to_string(long_operation_point.datetime)],
-                "short_signals": [
-                    datetime_to_string(short_operation_point.datetime),
-                    datetime_to_string(short_operation_point_2.datetime),
-                ],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point_2.datetime),
-    }
-
-    response = requests.post(
-        url=endpoint("process_strategies"),
-        json=body,
-        timeout=5,
-    )
-
-    response_content = parse_response(response)
-
-    assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 1.5
-    assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
-    )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
-
-
-def test_annual_operation_count_with_partial_operation_point_selection_3(  # noqa: PLR0913
-    long_operation_point,
-    long_operation_point_2,
-    short_operation_point,
-    short_operation_point_2,
-    endpoint,
-    session,
-):
-    body = {
-        "operation_points": {
-            10: {  # money_management_strategy_id
-                "long_operation_points": {
-                    datetime_to_string(
-                        long_operation_point.datetime
-                    ): long_operation_point.to_request_format(),
-                    datetime_to_string(
-                        long_operation_point_2.datetime
-                    ): long_operation_point_2.to_request_format(),
-                },
-                "short_operation_points": {
-                    datetime_to_string(
-                        short_operation_point.datetime
-                    ): short_operation_point.to_request_format(),
-                    datetime_to_string(
-                        short_operation_point_2.datetime
-                    ): short_operation_point_2.to_request_format(),
-                },
-            },
-        },
-        "signals": {
-            20: {  # indicator_id
-                "long_signals": [
-                    datetime_to_string(long_operation_point.datetime),
-                    datetime_to_string(long_operation_point_2.datetime),
-                ],
-                "short_signals": [
-                    datetime_to_string(short_operation_point.datetime),
-                    datetime_to_string(short_operation_point_2.datetime),
-                ],
-            },
-        },
-        "start_date": datetime_to_string(long_operation_point.datetime),
-        "end_date": datetime_to_string(short_operation_point_2.datetime),
-    }
-
-    response = requests.post(
-        url=endpoint("process_strategies"),
-        json=body,
-        timeout=5,
-    )
-
-    response_content = parse_response(response)
-
-    assert len(response_content["data"]) == 1
-    assert response_content["data"][0]["strategy_data"]["annual_operation_count"] == 2.0
-    assert (
-        response_content["data"][0]["strategy_data"]["money_management_strategy_id"] == 10
-    )
-    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 20
+    assert response_content["data"][0]["strategy_data"]["indicator_id"] == 701
