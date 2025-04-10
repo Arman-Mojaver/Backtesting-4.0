057984fd4c3679a154b4421b992b02185630ea6f
Arman Mojaver <arman_mojaver@hotmail.com>
2025-04-10 10:51:49 +0200

    models: remove LongOperationPointQuery and ShortOperationPointQuery

--- database/models/short_operation_points_strategies.py
+++ database/models/short_operation_points_strategies.py
@@ -2,21 +2,13 @@ from __future__ import annotations
 
 from sqlalchemy import Column, Integer
 
-from database import Base, CRUDMixin, session
-
-
-class ShortOperationPointQuery:
-    @staticmethod
-    def all() -> list[ShortOperationPointStrategy]:
-        return list(session.query(ShortOperationPointStrategy).all())
+from database import Base, CRUDMixin
 
 
 class ShortOperationPointStrategy(Base, CRUDMixin):
     __tablename__ = "short_operation_points_strategies"
     __repr_fields__ = ("short_operation_point_id", "strategy_id")
     serialize_rules = ("-short_operation_point_id", "-strategy_id")
-
-    query = ShortOperationPointQuery
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     short_operation_point_id = Column(Integer, nullable=False)
