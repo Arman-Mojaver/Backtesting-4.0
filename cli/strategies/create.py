ace56e7c92d8f68eadff130750e5ee830db05975
Arman Mojaver <arman_mojaver@hotmail.com>
2025-04-10 16:29:20 +0200

    cli: add instrument validation to create strategies command

--- cli/strategies/create.py
+++ cli/strategies/create.py
@@ -11,11 +11,17 @@ from logger import log
 @click.command("create", help="Create Strategies")
 @click.option("-i", "--instrument", type=str, required=True, help="Instrument")
 def create_strategies(instrument: str) -> None:
+    instrument = instrument.upper()
+
     if config.is_production():
         confirm(
             "You are about to create strategies in production. "
             "Do you wish to continue?"
         )
+
+    if instrument not in config.ENABLED_INSTRUMENTS:
+        err = f"Instrument not supported: {instrument}. {config.ENABLED_INSTRUMENTS=}"
+        raise click.ClickException(err)
 
     log.info(f"Created strategies for instrument: {instrument}")
 
