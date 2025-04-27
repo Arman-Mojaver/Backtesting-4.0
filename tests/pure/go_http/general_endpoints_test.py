2795d1bffc5eceff0d425674dc2569dc06d5c49e
Arman Mojaver <arman_mojaver@hotmail.com>
2025-04-27 14:45:01 +0100

    tests: split general endpoints tests into separate file

---
+++ tests/pure/go_http/general_endpoints_test.py
@@ -0,0 +1,17 @@
+import requests
+
+from testing_utils.http_utils import parse_response
+
+
+def test_non_existent_endpoint(endpoint):
+    response = requests.get(endpoint("non_existent_endpoint"), timeout=5)
+
+    assert parse_response(response) == {
+        "message": "Server Working. Endpoint not defined!"
+    }
+
+
+def test_ping(endpoint):
+    response = requests.get(endpoint("ping"), timeout=5)
+
+    assert parse_response(response) == {"message": "Ping!"}
