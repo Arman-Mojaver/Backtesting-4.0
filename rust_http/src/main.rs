c51126a6d0ea5981e58818fb36b78e5a1a50ffe9
Arman Mojaver <arman_mojaver@hotmail.com>
2025-05-05 23:09:47 +0100

    rust_http: add logging

--- rust_http/src/main.rs
+++ rust_http/src/main.rs
@@ -1,5 +1,9 @@
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+use chrono::Local;
+use fern::colors::{Color, ColoredLevelConfig};
+use log::info;
 use serde::Serialize;
+use std::io;
 
 async fn index() -> impl Responder {
     HttpResponse::NotFound().json(PingResponse {
@@ -14,15 +18,69 @@ struct PingResponse {
 
 #[get("/ping")]
 async fn ping() -> impl Responder {
+    info!("GET /ping");
     web::Json(PingResponse {
         message: "Ping!".into(),
     })
 }
 
+// -- logger setup --
+fn setup_logger() -> Result<(), fern::InitError> {
+    let colors = ColoredLevelConfig::new()
+        .error(Color::Red)
+        .warn(Color::Yellow)
+        .info(Color::Green)
+        .debug(Color::Blue)
+        .trace(Color::Magenta);
+
+    let log_file = fern::log_file("logs/rust-http.log")?;
+
+    fern::Dispatch::new()
+        .level(log::LevelFilter::Off)
+        .level_for("rust_http", log::LevelFilter::Info)
+        .chain(
+            fern::Dispatch::new()
+                .format(move |out, message, record| {
+                    out.finish(format_args!(
+                        "{} [{}] [{}:{}] {}",
+                        Local::now().format("%Y/%m/%d %H:%M:%S"),
+                        colors.color(record.level()),
+                        record.file().unwrap_or("unknown"),
+                        record.line().unwrap_or(0),
+                        message
+                    ))
+                })
+                .chain(io::stdout()),
+        )
+        .chain(
+            fern::Dispatch::new()
+                .format(move |out, message, record| {
+                    out.finish(format_args!(
+                        "{} [{}] [{}:{}] {}",
+                        Local::now().format("%Y/%m/%d %H:%M:%S"),
+                        record.level(), // no ANSI color in file
+                        record.file().unwrap_or("unknown"),
+                        record.line().unwrap_or(0),
+                        message
+                    ))
+                })
+                .chain(log_file),
+        )
+        .apply()?;
+
+    Ok(())
+}
+
 #[actix_web::main]
-async fn main() -> std::io::Result<()> {
+async fn main() -> io::Result<()> {
+    // Initialize logger
+    if let Err(e) = setup_logger() {
+        eprintln!("Error initializing logger: {}", e);
+        std::process::exit(1);
+    }
+
     let host = std::env::var("HOST").unwrap_or_else(|_| "0.0.0.0:81".into());
-    println!("Server starting on port {}...", &host);
+    info!("Server starting on {}", &host);
 
     HttpServer::new(|| {
         App::new()
@@ -30,7 +88,7 @@ async fn main() -> std::io::Result<()> {
             .service(ping)
     })
     .workers(7)
-    .bind(host)?
+    .bind(&host)?
     .run()
     .await
 }
