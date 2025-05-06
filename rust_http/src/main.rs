759e5eaaa6cbc521419ca1098653f67d4196309c
Arman Mojaver <arman_mojaver@hotmail.com>
2025-05-06 21:06:51 +0100

    rust_http: modularize endpoints

--- rust_http/src/main.rs
+++ rust_http/src/main.rs
@@ -1,10 +1,12 @@
-use actix_web::{App, HttpServer};
+use actix_web::{web, App, HttpResponse, HttpServer};
 use chrono::Local;
 use fern::colors::{Color, ColoredLevelConfig};
 use log::info;
 use std::io;
 
+mod indicators;
 mod routes;
+mod strategies;
 
 // -- logger setup --
 fn setup_logger() -> Result<(), fern::InitError> {
@@ -53,6 +55,45 @@ fn setup_logger() -> Result<(), fern::InitError> {
     Ok(())
 }
 
+fn configure_routes(cfg: &mut web::ServiceConfig) {
+    cfg
+        // process_strategies
+        .route(
+            "/process_strategies",
+            web::post().to(strategies::process_strategies),
+        )
+        .route(
+            "/process_strategies",
+            web::get().to(routes::method_not_allowed),
+        )
+        .route(
+            "/process_strategies",
+            web::put().to(routes::method_not_allowed),
+        )
+        .route(
+            "/process_strategies",
+            web::delete().to(routes::method_not_allowed),
+        )
+        // rsi
+        .route("/rsi", web::post().to(indicators::rsi::rsi))
+        .route("/rsi", web::get().to(routes::method_not_allowed))
+        .route("/rsi", web::put().to(routes::method_not_allowed))
+        .route("/rsi", web::delete().to(routes::method_not_allowed))
+        // General routes
+        .service(routes::ping)
+        .default_service(web::route().to(routes::index))
+        // Invalid JSON error handler
+        .app_data(web::JsonConfig::default().error_handler(|err, _req| {
+            actix_web::error::InternalError::from_response(
+                err,
+                HttpResponse::BadRequest().json(serde_json::json!({
+                    "error": "Invalid JSON"
+                })),
+            )
+            .into()
+        }));
+}
+
 #[actix_web::main]
 async fn main() -> io::Result<()> {
     // Initialize logger
@@ -64,7 +105,7 @@ async fn main() -> io::Result<()> {
     let host = std::env::var("HOST").unwrap_or_else(|_| "0.0.0.0:81".into());
     info!("Server starting on {}", &host);
 
-    HttpServer::new(|| App::new().configure(routes::configure_routes))
+    HttpServer::new(|| App::new().configure(configure_routes))
         .workers(7)
         .bind(&host)?
         .run()
--- rust_http/src/routes.rs
+++ rust_http/src/routes.rs
@@ -1,25 +1,14 @@
 use actix_web::{get, web, HttpResponse, Responder};
 use log::info;
-use serde::{Deserialize, Serialize};
+use serde::Serialize;
 
-#[derive(Debug, Deserialize)]
-struct RequestPayload {}
-
-async fn process_strategies(_request_body: web::Json<RequestPayload>) -> impl Responder {
-    HttpResponse::Ok().json(serde_json::json!({ "data": [] }))
-}
-
-async fn rsi(_request_body: web::Json<RequestPayload>) -> impl Responder {
-    HttpResponse::Ok().json(serde_json::json!({ "data": [] }))
-}
-
-async fn method_not_allowed() -> impl Responder {
+pub async fn method_not_allowed() -> impl Responder {
     HttpResponse::MethodNotAllowed().json(serde_json::json!({
         "error": "Method Not Allowed"
     }))
 }
 
-async fn index() -> impl Responder {
+pub async fn index() -> impl Responder {
     HttpResponse::NotFound().json(PingResponse {
         message: "Server Working. Endpoint not defined!".into(),
     })
@@ -31,36 +20,9 @@ struct PingResponse {
 }
 
 #[get("/ping")]
-async fn ping() -> impl Responder {
+pub async fn ping() -> impl Responder {
     info!("GET /ping");
     web::Json(PingResponse {
         message: "Ping!".into(),
     })
-}
-
-pub fn configure_routes(cfg: &mut web::ServiceConfig) {
-    cfg
-        // process_strategies
-        .route("/process_strategies", web::post().to(process_strategies))
-        .route("/process_strategies", web::get().to(method_not_allowed))
-        .route("/process_strategies", web::put().to(method_not_allowed))
-        .route("/process_strategies", web::delete().to(method_not_allowed))
-        // rsi
-        .route("/rsi", web::post().to(rsi))
-        .route("/rsi", web::get().to(method_not_allowed))
-        .route("/rsi", web::put().to(method_not_allowed))
-        .route("/rsi", web::delete().to(method_not_allowed))
-        // General routes
-        .service(ping)
-        .default_service(web::route().to(index))
-        // Invalid JSON error handler
-        .app_data(web::JsonConfig::default().error_handler(|err, _req| {
-            actix_web::error::InternalError::from_response(
-                err,
-                HttpResponse::BadRequest().json(serde_json::json!({
-                    "error": "Invalid JSON"
-                })),
-            )
-            .into()
-        }));
 }
