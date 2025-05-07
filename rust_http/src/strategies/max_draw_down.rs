ef4476feaca330fa72f69442116ca3c39bd1273e
Arman Mojaver <arman_mojaver@hotmail.com>
2025-05-07 21:16:46 +0100

    rust_http: add functionality to max_draw_down endpoint

--- rust_http/src/strategies/max_draw_down.rs
+++ rust_http/src/strategies/max_draw_down.rs
@@ -13,5 +13,25 @@ pub async fn max_draw_down(payload: web::Json<MaxDrawDownPayload>) -> impl Respo
 }
 
 pub fn get_max_draw_down(operation_points: &Vec<OperationPoint>) -> f64 {
-    0.0
+    let mut cumsum: f64 = 1.0;
+    let mut cummax: f64 = 1.0;
+    let mut draw_downs: Vec<f64> = Vec::new();
+
+    for point in operation_points.iter() {
+        let result: f64 = point.result as f64 * point.risk / point.sl as f64;
+        cumsum += result;
+        cummax = cummax.max(cumsum);
+        let draw_down: f64 = (cummax - cumsum) / cummax;
+        let rounded_draw_down: f64 = (draw_down * 10000.0).round() / 100.0;
+        draw_downs.push(rounded_draw_down);
+    }
+    get_max(&draw_downs)
+}
+
+fn get_max(floats: &Vec<f64>) -> f64 {
+    let mut max_value: f64 = 0.0;
+    for float in floats.iter() {
+        max_value = max_value.max(*float);
+    }
+    max_value
 }
--- testing_utils/request_body_factory/operation_points_factory.py
+++ testing_utils/request_body_factory/operation_points_factory.py
@@ -90,3 +90,69 @@ class OperationPointsFactory:
             raise ValueError(err)
 
         return OperationPoints(operation_points)
+
+
+class OperationPointsFromDataFactory:
+    def __init__(
+        self,
+        instrument: str,
+        mm_strategy_id: int,
+        start_date: str,
+        data: tuple[tuple[int, int, int], ...],
+    ):
+        self.instrument: str = instrument
+        self.mm_strategy_id: int = mm_strategy_id
+        self.start_date: str = start_date
+        self.data: tuple[tuple[int, int, int], ...] = data
+        self.count: int = len(data)
+
+        self.long_operation_points, self.short_operation_points = (
+            self._generate_long_short_operation_points_list()
+        )
+        self.operation_points = self._generate_operation_points_list()
+
+    def _generate_operation_points_list(self) -> OperationPoints:
+        operation_points = []
+        for index, (long_point, short_point) in enumerate(
+            zip(self.long_operation_points, self.short_operation_points, strict=True)
+        ):
+            if index % 2 == 0:
+                operation_points.append(long_point)
+            else:
+                operation_points.append(short_point)
+
+        return OperationPoints(operation_points)
+
+    def _generate_long_short_operation_points_list(
+        self,
+    ) -> tuple[list[LongOperationPoint], list[ShortOperationPoint]]:
+        long_operation_points = generate_random_long_operation_points(
+            money_management_strategy_id=self.mm_strategy_id,
+            instrument=self.instrument,
+            start_date=self.start_date,
+            count=self.count,
+        )
+
+        short_operation_points = generate_random_short_operation_points(
+            money_management_strategy_id=self.mm_strategy_id,
+            instrument=self.instrument,
+            start_date=self.start_date,
+            count=self.count,
+        )
+
+        for index, point in enumerate(long_operation_points + short_operation_points, 1):
+            point.id = index
+
+        return long_operation_points, short_operation_points
+
+    def operation_points_from_data(self) -> OperationPoints:
+        if len(self.data) != len(self.operation_points):
+            err = "Mismatch between data and operation points sizes"
+            raise ValueError(err)
+
+        for index, (result, tp, sl) in enumerate(self.data):
+            self.operation_points[index].result = result
+            self.operation_points[index].tp = tp
+            self.operation_points[index].sl = sl
+
+        return OperationPoints(self.operation_points)
