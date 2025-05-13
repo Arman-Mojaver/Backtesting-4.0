6e7906bcdff065fe351230f63fa3eab8b6144b57
Arman Mojaver <arman_mojaver@hotmail.com>
2025-05-13 09:34:30 +0200

    rust_http: use vector references in get_global_roi()

--- rust_http/src/strategies/global_roi.rs
+++ rust_http/src/strategies/global_roi.rs
@@ -2,11 +2,12 @@ use crate::strategies::{OperationPoint, OperationPointsPayload};
 use actix_web::{web, HttpResponse, Responder};
 
 pub async fn global_roi(payload: web::Json<OperationPointsPayload>) -> impl Responder {
-    let global_roi: f64 = get_global_roi(&payload.operation_points);
+    let operation_point_references: Vec<&OperationPoint> = payload.operation_points.iter().collect();
+    let global_roi: f64 = get_global_roi(&operation_point_references);
     HttpResponse::Ok().json(serde_json::json!({ "data": global_roi }))
 }
 
-pub fn get_global_roi(operation_points: &Vec<OperationPoint>) -> f64 {
+pub fn get_global_roi(operation_points: &Vec<&OperationPoint>) -> f64 {
     let mut cumsum: f64 = 1.0;
     for point in operation_points {
         let result = point.result as f64 * (point.risk / point.sl as f64);
