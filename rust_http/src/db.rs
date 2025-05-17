faf8bc93a901fa62a427e16d07bb8843fd609c8e
Arman Mojaver <arman_mojaver@hotmail.com>
2025-05-17 17:50:07 +0200

    rust_http: set postgres max pool connections to 1

--- rust_http/src/db.rs
+++ rust_http/src/db.rs
@@ -4,7 +4,7 @@ use sqlx::{postgres::PgPoolOptions, Pool, Postgres, Result};
 pub async fn init_pool(db_config: DbConfig) -> Result<Pool<Postgres>> {
     let url = db_config.url();
     let pool = PgPoolOptions::new()
-        .max_connections(5)
+        .max_connections(1)
         .connect(&url)
         .await?;
     Ok(pool)
