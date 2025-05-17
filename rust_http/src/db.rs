40a2c647995c53d1748101be707809e66893fb23
Arman Mojaver <arman_mojaver@hotmail.com>
2025-05-17 11:54:59 +0200

    rust_http: add db.rs (init_pool())

---
+++ rust_http/src/db.rs
@@ -0,0 +1,11 @@
+use crate::config::DbConfig;
+use sqlx::{postgres::PgPoolOptions, Pool, Postgres, Result};
+
+pub async fn init_pool(db_config: DbConfig) -> Result<Pool<Postgres>> {
+    let url = db_config.url();
+    let pool = PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&url)
+        .await?;
+    Ok(pool)
+}
