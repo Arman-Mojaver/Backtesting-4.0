b3010b7799d1155c1ddd6efbe6f22afb80c9ee62
Arman Mojaver <arman_mojaver@hotmail.com>
2025-06-28 14:47:45 +0200

    tasks: refactor create_strategies_recursively() to call rust endpoint

--- tasks/create_strategies.py
+++ tasks/create_strategies.py
@@ -1,11 +1,11 @@
-from database.models import Indicator, MoneyManagementStrategy, ResampledPointD1, Strategy
+import requests
+
+from database import session
+from database.models import Indicator, MoneyManagementStrategy, Strategy
 from database.models.money_management_strategy import MoneyManagementStrategyList
 from logger import log
 from tasks.celery_app import celery
-from views.process_strategies.operation_points_validator import (
-    InvalidOperationPointsError,
-    OperationPointsValidator,
-)
+from testing_utils.http_utils import parse_response
 
 
 @celery.task(name="create_strategies_recursively")
@@ -19,32 +19,38 @@ def create_strategies_recursively(instrument: str) -> bool:
     ids = money_management_strategy_ids - strategy_mm_strategy_ids
     if not ids:
         log.info("No more unprocessed money management strategies")
+        log.info("Task Output: False")
         return False
 
     money_management_strategy_id = next(iter(ids))
     log.info(
         f"Processing Money Management Strategy with ID: {money_management_strategy_id}"
     )
-    queried_money_management_strategy = (
-        MoneyManagementStrategy.query.from_id(id=money_management_strategy_id)
-        .with_operation_points()
-        .one()
-    )
-    queried_resampled_points = ResampledPointD1.query.from_instrument(  # noqa: F841
-        instrument=instrument,
-    ).all()
-    queried_indicators = Indicator.query.all()  # noqa: F841
-
-    try:
-        operation_points = OperationPointsValidator(  # noqa: F841
-            money_management_strategy_id=money_management_strategy_id,
-            long_operation_points=queried_money_management_strategy.long_operation_points,
-            short_operation_points=queried_money_management_strategy.short_operation_points,
-        ).run()
-    except InvalidOperationPointsError as e:
-        log.exception(f"Invalid operation points: {e}")
+    indicator_types = [row[0] for row in session.query(Indicator.type).distinct().all()]
+    if len(indicator_types) != 1:
+        log.info(
+            f"There is more than one indicator type in the database: {indicator_types}"
+        )
+        log.info("Task Output: False")
         return False
 
-    log.info("Finished OperationPointsValidator")
+    indicator_type = next(iter(indicator_types))
 
+    data = {
+        "instrument": instrument,
+        "money_management_strategy_id": money_management_strategy_id,
+        "indicator_type": indicator_type,
+    }
+
+    log.info("Sending request")
+    response = requests.post(
+        url="http://host.docker.internal/process_strategies",
+        json=data,
+        timeout=300,
+    )
+    log.info("Processing response")
+    content = parse_response(response)
+    log.info(content)
+
+    log.info("Task Output: True")
     return True
