11bc14662e6e453840b9f03c75afb38c3cdde74a
Arman Mojaver <arman_mojaver@hotmail.com>
2025-06-28 14:53:07 +0200

    rust_http: remove min_connections() from db pool

--- rust_http/src/db.rs
+++ rust_http/src/db.rs
@@ -4,7 +4,6 @@ use sqlx::{postgres::PgPoolOptions, Pool, Postgres, Result};
 pub async fn init_pool(db_config: DbConfig) -> Result<Pool<Postgres>> {
     let url = db_config.url();
     let pool = PgPoolOptions::new()
-        .min_connections(6)
         .max_connections(6)
         .connect(&url)
         .await?;
