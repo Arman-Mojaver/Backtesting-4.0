7f898a51229b75db02f91963907648350256717f
Arman Mojaver <arman_mojaver@hotmail.com>
2025-06-28 14:46:14 +0200

    rust_http: add min_connections(6) and max_connections(6) to db pool

--- rust_http/src/db.rs
+++ rust_http/src/db.rs
@@ -4,7 +4,8 @@ use sqlx::{postgres::PgPoolOptions, Pool, Postgres, Result};
 pub async fn init_pool(db_config: DbConfig) -> Result<Pool<Postgres>> {
     let url = db_config.url();
     let pool = PgPoolOptions::new()
-        .max_connections(1)
+        .min_connections(6)
+        .max_connections(6)
         .connect(&url)
         .await?;
     Ok(pool)
